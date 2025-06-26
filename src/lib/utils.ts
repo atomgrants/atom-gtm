087b6f03882a90b2528719b1213ab2a1e23d22fb
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-06-26 11:11:13 -0400

    added discord handler

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,6 +7,9 @@ import { supabaseAdmin } from '@/lib/supabase';
 import { EmailInsert } from '@/types/email';
 import { NextResponse } from 'next/server';
 
+//const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = false;
+
 /** Merge classes with tailwind-merge with clsx full feature */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -40,6 +43,18 @@ export const insertEmail = async (email: EmailInsert) => {
   }
 
   console.log('Email inserted:', data);
+
+  //discord notification
+  if(isProduction){
+    await discordNotification({
+      email: data[0].sender_email_address,
+      name: data[0].sender_name,
+      subject: data[0].subject,
+      message: data[0].body,
+    });
+    console.log('Discord notification sent');
+  }
+
   return NextResponse.json({
     success: true,
     message: 'Email inserted',
@@ -127,3 +142,39 @@ async function getEmailsSinceDate(gmail: any, sinceDate: Date) {
   );
 }
 
+
+export const discordNotification = async ({
+  email,
+  name,
+  subject,
+  message,
+}: {
+  email: string;
+  name: string;
+  subject: string;
+  message: string;
+}) => {
+  if (!isProduction) return;
+
+  return await fetch(
+    `https://discord.com/api/webhooks/1387797986817609728/GPyC21ZUU5ZQd-_R4HnqXZoMRCzVBjrxDRzziRIuUhZFooxUh9CxM1qoITCn2dQPxLjE`,
+
+    {
+      body: JSON.stringify({
+        content: `## ❗️ New Support Request ❗️ \nName: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    }
+  )
+    .then(() => {
+      console.log('Discord notification fetch succeeded');
+      return true;
+    })
+    .catch((error) => {
+      console.error('Error sending discord notification', error);
+      return false;
+    });
+};

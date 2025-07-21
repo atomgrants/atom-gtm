e0da139d72cd6fd47495ae1781b8c3d4789b7a02
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-07-21 10:00:30 -0400

    set up endpoint for fetch-job

--- src/app/api/jobpost/fetch-jobs/route.ts
+++ src/app/api/jobpost/fetch-jobs/route.ts
@@ -1,24 +1,23 @@
 import { NextResponse } from 'next/server';
 
-import { contentKeywordFilter } from '@/lib/emailUtils';
 import { supabaseAdmin } from '@/lib/supabase';
 
 import { jobKeywords } from '@/data/keywords';
 
 export async function GET(request: Request) {
+  const orFilter = jobKeywords.map((keyword) => `subject.ilike.%${keyword}%`).join(',');
   const { data, error } = await supabaseAdmin
     .from('emails')
     .select('*')
-    .eq('listserv_name', 'ResAdmin')
-    .eq('subject', 'Job Posting');
+    .or(orFilter)
+    .limit(1)
 
   if (error) {
     console.error(error);
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 
-  const jobPosts = data.filter((email) => contentKeywordFilter(email.body, email.subject, jobKeywords));
-  console.log(jobPosts);
+  console.log({data, error});
 
   return NextResponse.json({ success: true });
 }

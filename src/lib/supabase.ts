b008bad936c14ae71f3c3caa6f3d7ab9dfa14317
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-07-25 13:05:58 -0400

    job db insert working

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv'
+
+dotenv.config({path:['.env.local', '.env']})
 
 const supabaseAdmin = createClient(
   process.env.SUPABASE_URL!,

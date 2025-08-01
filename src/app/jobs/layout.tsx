1523ab04bcc61540bed7a5454efa8e9ec64fd3c0
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-08-01 17:07:16 -0400

    setting up search option

--- src/app/jobs/layout.tsx
+++ src/app/jobs/layout.tsx
@@ -1,23 +1,29 @@
 import Head from 'next/head';
+
 import BackToJobButton from '@/components/buttons/BackToJobButton';
+import { Input } from '@/components/ui/input';
 
 export default function JobLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <body>
         <div className='flex flex-col min-h-screen bg-white'>
-          <main className='flex-1 flex-col items-center'>
+          <main className='flex flex-col items-center'>
             <Head>
               <title>Atom Job Posting Board</title>
             </Head>
-            <header className='relative'>
-              <div className='absolute left-10'>
+            <div className='flex flex-row justify-between items-center w-full px-10 mt-10'>
+              <div className='w-40'>
                 <BackToJobButton />
               </div>
-              <h1 className='mt-10 text-center antialiased font-bold leading-tight text-black text-4xl'>
+              <h1 className='text-center antialiased font-bold leading-tight text-black text-4xl flex-1'>
                 Atom Job Postings
               </h1>
-            </header>
+              <div className='w-36'>
+                <Input type='search' placeholder='search job' />
+              {/*<SearchJob/>*/}
+              </div>
+            </div>
             {children}
           </main>
         </div>
